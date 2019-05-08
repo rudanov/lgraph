750819461a7679a0df07fd8ed513d9e9be13b26f
rudanov <rudanov.d@gmail.com>
2019-05-09 01:35:42 +0300

    PyPi package init

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='lgraph',
+     version='0.1',
+     scripts=['lgraph'],
+     author="Dmitry Rudanov",
+     author_email="rudanov.d@gmail.com",
+     description="Python module for working with L-graphs",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/rudanov/lgraph",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
